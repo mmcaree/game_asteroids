3557974dd0c0521bc16525fadd3de8d778b653cf
mmcaree <mmcaree007@gmail.com>
2025-02-24 18:03:24 -0500

    updated main.py to set screen and render black surface

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-import pygame
+import pygame as p
 from constants import *
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    p.init()
+    screen_set = p.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in p.event.get():
+            if event.type == p.QUIT:
+                return
+            
+        p.Surface.fill(screen_set,(0,0,0))
+        p.display.flip()
 
 if __name__ == "__main__":
     main()
